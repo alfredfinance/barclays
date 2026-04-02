d041e6cdd2de814890cbf04cea8d9917da9f360a
alfredfinance <alfredfinance1per@gmail.com>
2026-04-02 11:14:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ for card in job_cards:
 
 driver.quit()
 print(f"Collected {len(job_urls)} job URLs")
-print(job_urls.head(5))
+print(job_urls[:5])
 
 
 #------------------------CHECK DUPLICATES URL DANS BIGQUERY--------------------------------------------------
